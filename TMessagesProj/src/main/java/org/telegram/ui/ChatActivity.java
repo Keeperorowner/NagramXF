f76c1f38ecf959cba3499a48f39fff038d6c981b
Keeperorowner <172187161+Keeperorowner@users.noreply.github.com>
2026-04-25 22:07:07 +0800

    fix: use INVISIBLE instead of GONE for chatInputViewsContainer when bottom bar is hidden

--- TMessagesProj/src/main/java/org/telegram/ui/ChatActivity.java
+++ TMessagesProj/src/main/java/org/telegram/ui/ChatActivity.java
@@ -29215,14 +29215,12 @@ public class ChatActivity extends BaseFragment implements
 
         if (shouldHideBottomBar()) {
             bottomOverlayChatText.setText("");
-            chatInputViewsContainer.setVisibility(View.GONE);
-            chatInputViewsContainer.setBackgroundWithFadeDrawable(null);
-            chatInputViewsContainer.getFadeView().invalidate();
+            chatInputViewsContainer.setVisibility(View.INVISIBLE);
         } else {
             chatInputViewsContainer.setVisibility(View.VISIBLE);
-            chatInputViewsContainer.setBackgroundWithFadeDrawable(fadeDrawable);
-            chatInputViewsContainer.getFadeView().invalidate();
         }
+        chatInputViewsContainer.setBackgroundWithFadeDrawable(fadeDrawable);
+        chatInputViewsContainer.getFadeView().invalidate();
 
         checkRaiseSensors();
     }
