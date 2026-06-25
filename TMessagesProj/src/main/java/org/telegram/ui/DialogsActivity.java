7486e01ba71cd5371d2025ed391823d64b4a69d4
Keeperorowner <1832713361@qq.com>
2026-06-25 21:25:04 +0800

    fix: update blur glass sources when folders are pinned to bottom

--- TMessagesProj/src/main/java/org/telegram/ui/DialogsActivity.java
+++ TMessagesProj/src/main/java/org/telegram/ui/DialogsActivity.java
@@ -14586,6 +14586,14 @@ public class DialogsActivity extends BaseFragment implements NotificationCenter.
                 iBlur3Positions,
                 scrollableViewNoiseSuppressor
             );
+            if (iBlur3SourceGlassFrosted != null) {
+                iBlur3SourceGlassFrosted.setSize(fragmentView.getMeasuredWidth(), fragmentView.getMeasuredHeight());
+                iBlur3SourceGlassFrosted.updateDisplayListIfNeeded();
+            }
+            if (iBlur3SourceGlass != null) {
+                iBlur3SourceGlass.setSize(fragmentView.getMeasuredWidth(), fragmentView.getMeasuredHeight());
+                iBlur3SourceGlass.updateDisplayListIfNeeded();
+            }
             return;
         }
 
