4c19b7a71299076857c32bab5e0e1ffef6cd1ca8
Keeperorowner <1832713361@qq.com>
2026-06-27 00:52:31 +0800

    fix: include Google map provider in secret chat preview options

--- TMessagesProj/src/main/java/org/telegram/ui/Components/AlertsCreator.java
+++ TMessagesProj/src/main/java/org/telegram/ui/Components/AlertsCreator.java
@@ -1576,7 +1576,7 @@ public class AlertsCreator {
     public static AlertDialog showSecretLocationAlert(Context context, int currentAccount, final Runnable onSelectRunnable, boolean inChat, Theme.ResourcesProvider resourcesProvider) {
         ArrayList<String> arrayList = new ArrayList<>();
         ArrayList<Integer> types = new ArrayList<>();
-        int providers = 5; // MessagesController.getInstance(currentAccount).availableMapProviders;
+        int providers = 7; // MessagesController.getInstance(currentAccount).availableMapProviders;
         if ((providers & 1) != 0) {
             arrayList.add(LocaleController.getString(R.string.MapPreviewProviderTelegram));
             types.add(0);
