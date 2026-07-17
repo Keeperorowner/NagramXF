81c0c95140500cdb2562e7a005a79f3e9a0798c4
Keeperorowner <172187161+Keeperorowner@users.noreply.github.com>
2026-07-17 19:17:59 +0800

    update to 12.9.0 (6966)

--- TMessagesProj/src/main/java/org/telegram/ui/Cells/UserCell.java
+++ TMessagesProj/src/main/java/org/telegram/ui/Cells/UserCell.java
@@ -41,6 +41,7 @@ import org.telegram.messenger.NotificationCenter;
 import org.telegram.messenger.R;
 import org.telegram.messenger.UserConfig;
 import org.telegram.messenger.UserObject;
+import org.telegram.messenger.utils.DrawableUtils;
 import org.telegram.tgnet.ConnectionsManager;
 import org.telegram.tgnet.TLRPC;
 import org.telegram.ui.ActionBar.BaseFragment;
@@ -64,7 +65,7 @@ import org.telegram.ui.Stories.StoriesUtilities;
 
 import tw.nekomimi.nekogram.NekoConfig;
 
-public class UserCell extends FrameLayout implements NotificationCenter.NotificationCenterDelegate {
+public class UserCell extends FrameLayout implements NotificationCenter.NotificationCenterDelegate, Theme.Colorable {
 
     public BackupImageView avatarImageView;
     protected SimpleTextView nameTextView;
@@ -121,6 +122,7 @@ public class UserCell extends FrameLayout implements NotificationCenter.Notifica
     }
 
     protected long dialogId;
+    private boolean isCommunity;
 
     public UserCell(Context context, int padding, int checkbox, boolean admin) {
         this(context, padding, checkbox, admin, false, null, false);
@@ -534,6 +536,7 @@ public class UserCell extends FrameLayout implements NotificationCenter.Notifica
         TLRPC.User currentUser = null;
         TLRPC.Chat currentChat = null;
         dialogId = 0;
+        isCommunity = false;
         if (currentObject instanceof TLRPC.User) {
             currentUser = (TLRPC.User) currentObject;
             if (currentUser.photo != null) {
@@ -546,6 +549,7 @@ public class UserCell extends FrameLayout implements NotificationCenter.Notifica
                 photo = currentChat.photo.photo_small;
             }
             dialogId = currentChat.id;
+            isCommunity = ChatObject.isCommunity(currentChat);
         }
 
         if (mask != 0) {
@@ -757,16 +761,28 @@ public class UserCell extends FrameLayout implements NotificationCenter.Notifica
             avatarImageView.setImageDrawable(avatarDrawable);
         }
 
-        avatarImageView.setRoundRadius(org.telegram.messenger.AvatarCornerHelper.getAvatarRoundRadius(
-                46.0f,
-                currentChat != null && currentChat.forum,
-                currentChat != null && currentChat.forum && MessagesController.getInstance(currentAccount).getStoriesController().hasStories(dialogId)
-        ));
+        avatarImageView.setRoundRadius(isCommunity ? dp(46 * 20 / 72f) :
+                (currentChat != null && currentChat.forum ? dp(14) : dp(24)));
 
         nameTextView.setTextColor(Theme.getColor(Theme.key_windowBackgroundWhiteBlackText, resourcesProvider));
         if (mutualView != null) {
             mutualView.setVisibility(currentUser != null && currentUser.mutual_contact ? VISIBLE : GONE);
         }
+    }
+
+    @Override
+    protected boolean drawChild(@NonNull Canvas canvas, View child, long drawingTime) {
+        if (isCommunity && child == avatarImageView) {
+            DrawableUtils.drawCommunityCardDrawable(canvas, Theme.dialogs_communityCardsDrawable,
+                child.getX() + child.getWidth() / 2f,
+                child.getY() + child.getHeight() / 2f,
+                child.getHeight());
+        }
+        return super.drawChild(canvas, child, drawingTime);
+    }
+
+    @Override
+    public void updateColors() {
     }
 
     public void setSelfAsSavedMessages(boolean value) {
