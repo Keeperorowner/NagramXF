e03bce082a3ea46be3fbde9935ae611e9708be86
Keeperorowner <172187161+Keeperorowner@users.noreply.github.com>
2026-07-17 17:11:44 +0800

    update to 12.8.2 (6920)

--- TMessagesProj/src/main/java/org/telegram/ui/ActionBar/BaseFragment.java
+++ TMessagesProj/src/main/java/org/telegram/ui/ActionBar/BaseFragment.java
@@ -59,6 +59,7 @@ import org.telegram.messenger.UserConfig;
 import org.telegram.messenger.utils.LeakDetector;
 import org.telegram.tgnet.ConnectionsManager;
 import org.telegram.ui.ArticleViewer;
+import org.telegram.ui.Components.Bulletin;
 import org.telegram.ui.Components.BulletinFactory;
 import org.telegram.ui.Components.LayoutHelper;
 import org.telegram.ui.LaunchActivity;
@@ -491,7 +492,7 @@ public abstract class BaseFragment {
         }
 
         if (hasForceLightStatusBar() && !AndroidUtilities.isTablet() && getParentLayout().getLastFragment() == this && getParentActivity() != null && !finishing) {
-            AndroidUtilities.setLightStatusBar(getParentActivity().getWindow(), ColorUtils.calculateLuminance(Theme.getColor(Theme.key_actionBarDefault)) > 0.7f);
+            AndroidUtilities.setLightStatusBar(getParentActivity(), ColorUtils.calculateLuminance(Theme.getColor(Theme.key_actionBarDefault)) > 0.7f);
         }
 
         if (sheetsStack != null) {
@@ -1016,7 +1017,7 @@ public abstract class BaseFragment {
                 } else {
                     AndroidUtilities.setLightNavigationBar(bottomSheet[0], true);
                 }
-                AndroidUtilities.setLightStatusBar(getWindow(), fragment.isLightStatusBar());
+                AndroidUtilities.setLightStatusBar(this, fragment.isLightStatusBar());
                 fragment.onBottomSheetCreated();
             }
 
@@ -1453,6 +1454,19 @@ public abstract class BaseFragment {
     public void onInsets(int left, int top, int right, int bottom) {
 
     }
+
+
+    private Bulletin.Delegate bulletinDelegate;
+
+    public void setBulletinDelegate(Bulletin.Delegate bulletinDelegate) {
+        this.bulletinDelegate = bulletinDelegate;
+    }
+
+    public Bulletin.Delegate getBulletinDelegate() {
+        return bulletinDelegate;
+    }
+
+
     protected void dumpCanvas() {
         AndroidUtilities.dumpCanvas(fragmentView);
     }
